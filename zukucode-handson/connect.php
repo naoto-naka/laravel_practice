55d8d7f81369b3353e4e82abddc9c490701116bd
naoto N <n@MacBook-Pro-N.local>
2020-05-01 21:57:37 +0900

    first commit

---
+++ zukucode-handson/connect.php
@@ -0,0 +1,14 @@
+<?php
+    try {
+        $dsn = 'mysql:host=db;dbname=docker_db;';
+        $db = new PDO($dsn, 'docker_user', 'docker_pass');
+
+        $sql = 'SELECT version();';
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        var_dump($result);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit;
+    }
